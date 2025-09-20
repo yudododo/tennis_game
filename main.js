6e41a56a17bcdb4d912efa0664f9960e59a948e7
yudododo <ae24856@gmail.com>
2025-09-20 12:18:54 +0800

    add demo.gif

--- main.js
+++ main.js
@@ -6,15 +6,15 @@ canvas.height = window.innerHeight;
 
 let ballX = 30;
 let ballY = 30;
-let ballSpeedX = 5;
-let ballSpeedY = 3;
+let ballSpeedX = 6;
+let ballSpeedY = 4;
 let paddle1Y = 250;
 let paddle2Y = 250;
 let player1Score = 0;
 let player2Score = 0;
-const winningScore = 1;
+const winningScore = 3;
 let isWin = false;
-const paddleHeight = 100;
+const paddleHeight = 150;
 const paddleThickness = 10;
   
 let framesPerSecond = 30;
@@ -34,9 +34,11 @@ canvas.addEventListener("mousemove", (e) =>{
   paddle1Y = mousePosition.y - paddleHeight / 2
 })
 canvas.addEventListener("mousedown", (e) =>{
-  player1Score = 0;
-  player2Score = 0;
-  isWin = false;
+  if(isWin){
+    player1Score = 0;
+    player2Score = 0;
+    isWin = false;
+  }
 })
 function drawNet(){
   for(i = 0; i < canvas.height; i+=40){
@@ -47,15 +49,17 @@ function draw(){
   // 黑色背景
   colorRect(0, 0, canvas.width, canvas.height, "black");
   if (isWin){
-    ctx.fillStyle = "white";
     if(player1Score >= winningScore){
-      ctx.font = "20px Arial";
+      ctx.fillStyle = "white";
+      ctx.font = "20px 'Press Start 2P'";
       ctx.fillText("You Won!", canvas.width/2 , canvas.height/2)
     }else{
-      ctx.font = "20px Arial";
+      ctx.fillStyle = "blue";
+      ctx.font = "20px 'Press Start 2P'";
       ctx.fillText("Computer Won!", canvas.width/2 , canvas.height/2)
     }
-    ctx.font = "30px Arial";
+    ctx.fillStyle = "white";
+    ctx.font = "30px 'Press Start 2P'";
     ctx.textAlign = "center";
     ctx.fillText("Click to continue", canvas.width/2, canvas.height-100)
     return;
@@ -67,9 +71,15 @@ function draw(){
   // 球拍
   colorRect(10, paddle1Y, paddleThickness, paddleHeight , "pink");
   colorRect(canvas.width - paddleThickness - 10, paddle2Y, paddleThickness, paddleHeight , "pink");
-  ctx.font = "20px Arial";
-  ctx.fillText(player1Score, 100, 100)
-  ctx.fillText(player2Score, canvas.width -100, 100)
+  // 玩家分數（靠左）
+  ctx.fillStyle = "white";
+  ctx.font = "15px 'Press Start 2P'";
+  ctx.textAlign = "left";   // 從座標往右畫
+  ctx.fillText(`Your Score: ${player1Score}`, 100, 100);
+  // 電腦分數（靠右）
+  ctx.fillStyle = "blue";
+  ctx.textAlign = "right";  // 從座標往左畫
+  ctx.fillText(`Computer Score: ${player2Score}`, canvas.width - 100, 100);
 }
 function ballReset(){
   if(player1Score >= winningScore || player2Score >= winningScore){
@@ -98,7 +108,7 @@ function move(){
       // 打到球拍偏上：deltaY 為負，球往上斜飛
       // 打到球拍偏下：deltaY 為正，球往下斜飛
       let deltaY = ballY - (paddle1Y + (paddleHeight / 2))
-      ballSpeedY = deltaY * 0.35;
+      ballSpeedY = deltaY * 0.45;
     } else{
       player2Score ++; // 加分之後才reset
       ballReset();
@@ -108,7 +118,7 @@ function move(){
     if(ballY > paddle2Y && ballY < paddle2Y + paddleHeight){
       ballSpeedX = -ballSpeedX ;
       let deltaY = ballY - (paddle2Y + (paddleHeight / 2))
-      ballSpeedY = deltaY * 0.35;
+      ballSpeedY = deltaY * 0.45;
     } else{
       player1Score ++; 
       ballReset();
@@ -123,10 +133,10 @@ function move(){
 function computermove(){
   const paddle2YCenter = paddle2Y + (paddleHeight / 2)
   // 如果球在拍子中心點的上下某個範圍之間，就不用移動
-  if(ballY > paddle2YCenter + 35){
-    paddle2Y += 6;
-  }else if(ballY < paddle2YCenter - 35){
-    paddle2Y -= 6;
+  if(ballY > paddle2YCenter + 40){
+    paddle2Y += 10;
+  }else if(ballY < paddle2YCenter - 40){
+    paddle2Y -= 10;
   }
 }
 function colorRect(topX, leftY, width, height, color){
@@ -140,8 +150,6 @@ function colorCircle(centerX, centerY, radius, color){
   ctx.arc(centerX, centerY, radius, 0, Math.PI*2, true); // 定義一個圓
   ctx.fill(); // 填色，把它畫出來
 }
-
-
 
 setInterval(function(){
   draw();
